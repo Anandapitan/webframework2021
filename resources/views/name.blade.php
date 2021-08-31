4cf4cf2d0995162a12ad00ee33c5202913972f10
Ananda <anandapitan@gmail.com>
2021-09-01 02:28:13 +0800

    Commit pertama: template bootstrap

---
+++ resources/views/name.blade.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <title>Tugas 1</title>
+</head>
+<body style="background-color: #C32148; font-family: Centaur;">
+
+<ul style="list-style-type: none; text-align: center; margin-top:250px; padding: 0; font-size:20px;">
+  <li style="display: inline;"><a style="text-decoration: none; color:white; padding-right: 10px;" href="#home">My</a></li>
+  <li style="display: inline;"><a style="text-decoration: none; color:white; padding-right: 10px;" href="#news">Name</a></li>
+  <li style="display: inline;"><a style="text-decoration: none; color:white;" href="#contact">Is</a></li>
+</ul>
+
+<h1 style="text-align: center; margin:0;padding: 0; color:#660000; font-size:60px;">Ananda</h1>
+</body>
+</html>
+
